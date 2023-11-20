7cc0d41b58b2a581b06992fc06a7ef6c8ba86214
Chanda Sukesh <chandasukesh@gmail.com>
2023-11-20 07:17:12 +0530

    Link React router dom ep 07

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h4>
+        <a href="/about"> About</a>
+      </h4>
+
+      <h4>
+        <Link to="/contact">Contact</Link>
+      </h4>
+    </div>
+  );
 };
 
 export default Header;
